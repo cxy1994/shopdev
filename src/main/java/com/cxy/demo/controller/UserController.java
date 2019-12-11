6399d8a58fcca3ede56a78b3dff36a12e139ef7a
chenxy7 <chenxy7@asiainfo.com>
2019-12-11 09:59:39 +0800

    no message

--- src/main/java/com/cxy/demo/controller/UserController.java
+++ src/main/java/com/cxy/demo/controller/UserController.java
@@ -17,7 +17,7 @@ import com.cxy.demo.service.interfaces.IUserService;
  *
  * @author cxy
  */
-@ContextConfiguration(locations = {"classpath:spring/config/spring-mybatis.xml"})
+@ContextConfiguration(locations = {"classpath:config/spring/spring-mybatis.xml"})
 @Controller
 @RequestMapping("/home")
 // 根节点url
